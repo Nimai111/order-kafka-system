612d663658ed3cf1cdfb9c97e99c7094b692ccc4
Nimai111 <nimaicharan00000@gmail.com>
2026-09-20 18:08:07 +0530

    feat: initialize order kafka system

---
+++ src/test/java/com/nimai/order_kafka_system/OrderKafkaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nimai.order_kafka_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderKafkaSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
