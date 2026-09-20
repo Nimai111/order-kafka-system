6583b9167f5deef767f8c260bfe3170e0f7f7339
Nimai111 <nimaicharan00000@gmail.com>
2026-09-20 22:16:24 +0530

    feat: add order entity

---
+++ src/main/java/com/nimai/order_kafka_system/order/Order.java
@@ -0,0 +1,34 @@
+package com.nimai.order_kafka_system.order;
+
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "orders")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Product name is required")
+    private String productName;
+
+    @NotNull(message = "Quantity is required")
+    @Min(value = 1, message = "Quantity must be at least 1")
+    private Integer quantity;
+
+    @NotNull(message = "Price is required")
+    @Min(value = 1, message = "Price must be greater than 0")
+    private Double price;
+}
